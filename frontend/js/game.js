8b89a889e6bd4ea4542b489cb301a7c969471e67
Gol_D_Roger <149899338+NewForest2005@users.noreply.github.com>
2026-09-14 17:20:58 -0500

    (Draft) game.js

--- frontend/js/game.js
+++ frontend/js/game.js
@@ -1,1 +1,16 @@
 // TODO (Fase 2/3/8): orquesta el estado del juego, el "tick", el HUD y las colisiones.
+{
+   row: 7,
+   col: 9,
+   x: 228,
+   y: 180,
+   direction: "none",
+   nextDirection: "none",
+   speed: 2
+ }
+
+function createPlayer(spawn)
+function handlePlayerInput(keyCode)
+function updatePlayer(player, maze)
+function drawPlayer(player, cellSize)
+function resetPlayer(player, spawn)
