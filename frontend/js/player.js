1ee9ff080718fd6916644eaa07ccbabb65a665c9
Gol_D_Roger <149899338+NewForest2005@users.noreply.github.com>
2026-09-17 07:29:13 -0500

    Fronted basic implementation completed

--- frontend/js/player.js
+++ frontend/js/player.js
@@ -1,1 +1,41 @@
-// TODO (Fase 3): estado, movimiento y dibujo del jugador.
+function createPlayer(spawn) {
+  return {
+    row: spawn.row,
+    col: spawn.col,
+    x: cellCenter(spawn.col, game.maze.cellSize),
+    y: cellCenter(spawn.row, game.maze.cellSize),
+    direction: "none",
+    nextDirection: "none",
+    speed: 2
+  };
+}
+
+function handlePlayerInput(keyCode) {
+  const directions = {};
+  directions[UP_ARROW] = "up";
+  directions[DOWN_ARROW] = "down";
+  directions[LEFT_ARROW] = "left";
+  directions[RIGHT_ARROW] = "right";
+  if (game.player && directions[keyCode]) game.player.nextDirection = directions[keyCode];
+}
+
+function updatePlayer(player, maze) {
+  moveEntity(player, maze);
+}
+
+function drawPlayer(player, cellSize) {
+  push();
+  fill("#ffe600");
+  noStroke();
+  circle(player.x, player.y, cellSize * 0.72);
+  pop();
+}
+
+function resetPlayer(player, spawn) {
+  player.row = spawn.row;
+  player.col = spawn.col;
+  player.x = cellCenter(spawn.col, game.maze.cellSize);
+  player.y = cellCenter(spawn.row, game.maze.cellSize);
+  player.direction = "none";
+  player.nextDirection = "none";
+}
--- frontend/js/sketch.js
+++ frontend/js/sketch.js
@@ -1,7 +1,20 @@
+/* Use this only as the p5.js entry point.*/
+
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(19 * 24, 15 * 24 + 32);
+  initializeGame();
 }
 
 function draw() {
-  background(220);
+  background(0);
+
+  if (game.status === "playing") {
+    updateGame();
+  }
+
+  drawGame();
+}
+
+function keyPressed() {
+  handlePlayerInput(keyCode);
 }
